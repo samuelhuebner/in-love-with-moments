8e9e731ed40b7b013969b0584cee1b19a0bb6d2f
Samuel Hübner <samu.huebner@gmail.com>
2021-03-06 13:25:48 +0100

    feat(about): Adds about-me page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SwiperModule } from 'swiper/angular';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { HomeShowcaseComponent } from './containers/home-showcase/home-showcase.component';
+import { AboutMeComponent } from './containers/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    HomeShowcaseComponent
+    HomeShowcaseComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SwiperModule
+    SwiperModule,
+    LoadingBarRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
